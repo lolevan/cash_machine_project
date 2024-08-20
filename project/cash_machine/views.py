805aac4254558db0954c4adee8ff92ee6ba48a65
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-20 17:15:08 +0800

    Add cratch for pdfkit and fix bugs in naming files

--- project/cash_machine/views.py
+++ project/cash_machine/views.py
@@ -12,25 +12,34 @@ class CashMachineView(APIView):
         item_ids = request.data.get('items', [])
         items = Item.objects.filter(id__in=item_ids)
         total_price = sum(item.price for item in items)
-        current_time = now().strftime("%d.%m.%Y %H:%M")
+
+        # Формат для имени файла
+        file_time = now().strftime("%d.%m.%Y_%H-%M")
+        # Формат для отображения в чеке
+        display_time = now().strftime("%d.%m.%Y %H:%M")
+
         context = {
             'items': items,
             'total_price': total_price,
-            'current_time': current_time
+            'current_time': display_time,
         }
 
         # Генерация PDF с использованием Jinja2
         template = get_template('receipt_template.html')
         html = template.render(context)
-        pdf_file_path = f'media/receipt_{current_time}.pdf'
-        pdfkit.from_string(html, pdf_file_path)
+        pdf_file_path = f'media/receipt_{file_time}.pdf'
+
+        # Указываем путь к wkhtmltopdf
+        path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+        pdfkit.from_string(html, pdf_file_path, configuration=config)
 
         # Генерация QR-кода
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
         qr.add_data(pdf_file_path)
         qr.make(fit=True)
         img = qr.make_image(fill='black', back_color='white')
-        img_file_path = f'media/qrcode_{current_time}.png'
+        img_file_path = f'media/qrcode_{file_time}.png'
         img.save(img_file_path)
 
         return JsonResponse({'qr_code_url': img_file_path})
