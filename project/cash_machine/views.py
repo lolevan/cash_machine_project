bdf06f37f49b1c6849fe712ab6f6f830efb0d62c
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-20 17:22:03 +0800

    Add full path in output

--- project/cash_machine/views.py
+++ project/cash_machine/views.py
@@ -1,3 +1,4 @@
+import os
 import pdfkit
 import qrcode
 from django.http import JsonResponse
@@ -27,19 +28,27 @@ class CashMachineView(APIView):
         # Генерация PDF с использованием Jinja2
         template = get_template('receipt_template.html')
         html = template.render(context)
-        pdf_file_path = f'media/receipt_{file_time}.pdf'
+        pdf_file_name = f'receipt_{file_time}.pdf'
+        pdf_file_path = os.path.join('media', pdf_file_name)
 
         # Указываем путь к wkhtmltopdf
         path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
         config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
         pdfkit.from_string(html, pdf_file_path, configuration=config)
 
+        # Генерация полного пути к PDF файлу
+        pdf_url = request.build_absolute_uri(f'/media/{pdf_file_name}')
+
         # Генерация QR-кода
         qr = qrcode.QRCode(version=1, box_size=10, border=5)
-        qr.add_data(pdf_file_path)
+        qr.add_data(pdf_url)
         qr.make(fit=True)
         img = qr.make_image(fill='black', back_color='white')
-        img_file_path = f'media/qrcode_{file_time}.png'
+        qr_file_name = f'qrcode_{file_time}.png'
+        img_file_path = os.path.join('media', qr_file_name)
         img.save(img_file_path)
 
-        return JsonResponse({'qr_code_url': img_file_path})
+        # Генерация полного пути к QR-коду
+        qr_code_url = request.build_absolute_uri(f'/media/{qr_file_name}')
+
+        return JsonResponse({'qr_code_url': qr_code_url})
